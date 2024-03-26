90e9f2dbad3b317c97c5b4646adbe17330e9448a
Audren Burlot <audren.burlot@maxds.fr>
2024-03-26 07:57:54 +0100

    fixed normalize

--- src/project.ts
+++ src/project.ts
@@ -1,15 +1,15 @@
-import {makeProject} from '@motion-canvas/core';
-
-import example from './scenes/example?scene';
-import newton from './scenes/newton?scene';
-import newton2 from './scenes/newton2?scene';
-import newton3 from './scenes/newton3?scene';
-import logplusone from './scenes/logplusone?scene';
-import plusmu from './scenes/plusmu?scene';
-import pythagore from './scenes/pythagore?scene';
-import pythagore3D from './scenes/pythagore3D?scene';
+import { makeProject } from '@motion-canvas/core';
+// import example from './scenes/example?scene';
+// import newton from './scenes/newton?scene';
+// import newton2 from './scenes/newton2?scene';
+// import newton3 from './scenes/newton3?scene';
+// import logplusone from './scenes/logplusone?scene';
+// import plusmu from './scenes/plusmu?scene';
+// import pythagore from './scenes/pythagore?scene';
+// import pythagore3D from './scenes/pythagore3D?scene';
 import normalize from './scenes/normalize?scene';
 
+
 export default makeProject({
-  scenes: [plusmu],
+  scenes: [normalize],
 });
--- src/scenes/normalize.tsx
+++ src/scenes/normalize.tsx
@@ -3,7 +3,8 @@ import { createRef } from '@motion-canvas/core/lib/utils';
 import { all, waitFor } from '@motion-canvas/core/lib/flow';
 import { Grid, Latex, Node, Ray, Txt } from '@motion-canvas/2d/lib/components';
 import { Vector2 } from '@motion-canvas/core/lib/types';
-import { linear } from '@motion-canvas/core/lib/tweening';
+import { easeInOutQuad, linear } from '@motion-canvas/core/lib/tweening';
+import { createSignal } from '@motion-canvas/core/lib/signals';
 
 export default makeScene2D(function* (view) {
 	const rlw = 2;
@@ -21,23 +22,27 @@ export default makeScene2D(function* (view) {
 	const abs = createRef<Ray>();
 	const ord = createRef<Ray>();
 	const oneAbs = createRef<Ray>();
-	const twoAbs = createRef<Ray>();
 	const oneOrd = createRef<Ray>();
-	const twoOrd = createRef<Ray>();
 
 	const vector = createRef<Ray>();
 	const x = createRef<Ray>();
 	const y = createRef<Ray>();
 
 	const mText = createRef<Txt>();
+	const xText = createRef<Txt>();
+	const yText = createRef<Txt>();
+
+	const value = createSignal(0);
 
 	const textStyle = {
 		fontWeight: 500,
-		fontSize: 56,
+		fontSize: 50,
 		offsetY: -1,
 		padding: 20,
 		cache: true
 	};
+	const smallTextStyle = {...textStyle};
+	smallTextStyle.fontSize = 30;
 
 	view.add(
 		<Node ref={group}>
@@ -77,17 +82,8 @@ export default makeScene2D(function* (view) {
 				stroke={'#aaa'}
 				start={0}
 				end={1}
-				from={new Vector2(-82, 240)}
-				to={new Vector2(-82, 260)}
-			/>
-			<Ray
-				ref={twoAbs}
-				lineWidth={tickWidth}
-				stroke={'#aaa'}
-				start={0}
-				end={1}
-				from={new Vector2(82, 240)}
-				to={new Vector2(82, 260)}
+				from={new Vector2(0, 240)}
+				to={new Vector2(0, 260)}
 			/>
 			<Ray
 				ref={oneOrd}
@@ -95,26 +91,17 @@ export default makeScene2D(function* (view) {
 				stroke={'#aaa'}
 				start={0}
 				end={1}
-				from={new Vector2(-240, 82)}
-				to={new Vector2(-260, 82)}
+				from={new Vector2(-240, 0)}
+				to={new Vector2(-260, 0)}
 			/>
-			<Ray
-				ref={twoOrd}
-				lineWidth={tickWidth}
-				stroke={'#aaa'}
-				start={0}
-				end={1}
-				from={new Vector2(-240, -82)}
-				to={new Vector2(-260, -82)}
-			/>
-
+			
 			<Ray
 				ref={vector}
 				lineWidth={10}
 				endArrow
 				stroke={'#C878E1'}
 				from={new Vector2(-250, 250)}
-				to={new Vector2(170, -170)}
+				to={new Vector2(50, -50)}
 				start={0}
 				end={1}
 			/>
@@ -123,8 +110,8 @@ export default makeScene2D(function* (view) {
 				ref={x}
 				lineWidth={8}
 				stroke={xColor}
-				from={new Vector2(-repereSize, repereSize)}
-				to={new Vector2(165, 250)}
+				from={new Vector2(-repereSize, 250)}
+				to={new Vector2(50, 250)}
 				start={0}
 				end={1}
 			/>
@@ -133,33 +120,44 @@ export default makeScene2D(function* (view) {
 				lineWidth={8}
 				stroke={yColor}
 				from={new Vector2(-repereSize, repereSize)}
-				to={new Vector2(-repereSize, -165)}
+				to={new Vector2(-repereSize, -50)}
 				start={0}
 				end={1}
 			/>
 
-			<Txt ref={mText} text={'M = 2.5'} fill={'#C878E1'} x={0} y={120} opacity={1} {...textStyle} />
+			<Txt ref={mText} text={() => value().toFixed(2).toString()} fill={'#C878E1'} x={50} y={0} opacity={1} {...textStyle} />
+			<Txt ref={xText} text={() => Math.sqrt(value() / 2).toFixed(2).toString()} fill={xColor} x={50} y={repereSize} opacity={1} {...smallTextStyle} />
+			<Txt ref={yText} text={() => Math.sqrt(value() / 2).toFixed(2).toString()} fill={yColor} x={-repereSize - 50} y={-80} opacity={1} {...smallTextStyle} />
+
 		</Node>
 	);
 
 	yield group().position(0, 0);
 	yield group().scale(1.2);
-
+	yield value(2.5);
 	yield* waitFor(1);
 
+	// réduit
 	yield* all(...[
-		vector().to({ x: -80, y: 80 }, 2),
-		x().to({ x: -80, y: 250 }, 2),
-		y().to({ x: -250, y: 80 }, 2),
-		mText().text('M = 1.0', 2, linear)
+		vector().to({ x: -50, y: 50 }, 2),
+		x().to({ x: -50, y: 250 }, 2),
+		y().to({ x: -250, y: 50 }, 2),
+		mText().position({x: -50, y: 100}, 2),
+		xText().x(-50, 2),
+		yText().y(20, 2),
+		value(1, 2, easeInOutQuad)
 	]);
 
 	yield* waitFor(1);
 
+	// grandit
 	yield* all(...[
-		vector().to({ x: 170, y: -170 }, 2),
-		x().to({ x: 165, y: 250 }, 2),
-		y().to({ x: -repereSize, y: -165}, 2),
-		mText().text('M = 2.5', 2, linear)
+		vector().to({ x: 50, y: -50 }, 2),
+		x().to({ x: 50, y: 250 }, 2),
+		y().to({ x: -repereSize, y: -50}, 2),
+		mText().position({x: 50, y: 0}, 2),
+		xText().x(50, 2),
+		yText().y(-80, 2),
+		value(2.5, 2, easeInOutQuad)
 	]);
 });
